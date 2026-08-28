d1012d9058011eb6c90a73214dfea7c065eda0bd
suneetha29 <gantasuneetha36@gmail.com>
2026-08-28 15:46:44 +0530

    Add Bronze layer data loading script

--- sql/03_bronze_load.sql
+++ sql/03_bronze_load.sql
@@ -1,1 +1,29 @@
+-- ============================================
+-- Netflix Analytics
+-- Bronze Layer - Data Loading
+-- ============================================
 
+USE NetflixAnalytics;
+GO
+
+-- Load Netflix Content
+BULK INSERT bronze.netflix_content
+FROM 'C:\NetflixAnalytics\data\Netflix_30000_Unclean_Dataset.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDQUOTE = '"',
+    TABLOCK
+);
+GO
+
+-- Load Netflix User Activity
+BULK INSERT bronze.netflix_user_activity
+FROM 'C:\NetflixAnalytics\data\Netflix_User_Activity_30000_Unclean.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDQUOTE = '"',
+    TABLOCK
+);
+GO
